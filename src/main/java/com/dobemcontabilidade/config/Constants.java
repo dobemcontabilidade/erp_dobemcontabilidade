560612f2beb3fa00223d40c986e8bad92b2125f8
Elton Gonçalves <elton.jd.goncalves@gmail.com>
2024-08-09 19:23:16 -0300

    checkout api

---
+++ src/main/java/com/dobemcontabilidade/config/Constants.java
@@ -0,0 +1,12 @@
+package com.dobemcontabilidade.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+    public static final String DEFAULT_LANGUAGE = "pt-br";
+
+    private Constants() {}
+}
---
+++ src/test/java/com/dobemcontabilidade/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.dobemcontabilidade.security.jwt;
+
+import com.dobemcontabilidade.config.SecurityConfiguration;
+import com.dobemcontabilidade.config.SecurityJwtConfiguration;
+import com.dobemcontabilidade.config.WebConfigurer;
+import com.dobemcontabilidade.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+public @interface AuthenticationIntegrationTest {
+}
